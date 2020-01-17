ad5504162f3de42c0f34228ba4e5740c3f62220f
harrison.wu <harrison.wu.global@gmail.com>
2020-01-17 18:34:32 +0800

    Initial commit

---
+++ seven_tea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seven_tea
+//
+//  Created by harrison公司機 on 2020/1/17.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
