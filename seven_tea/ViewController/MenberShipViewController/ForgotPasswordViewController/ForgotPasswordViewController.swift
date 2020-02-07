487f4eb873145b20caa3dc03a33b2e3dfe05b7e3
harrison.wu <harrison.wu.global@gmail.com>
2020-02-07 16:47:26 +0800

    refs #32  createForgotPasswordViewController,motify SendRestForgotPasswordApi and ResetPasswordApi

---
+++ seven_tea/ViewController/MenberShipViewController/ForgotPasswordViewController/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/2/6.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ seven_tea/ViewController/MenberShipViewController/ForgotPasswordViewController/ResetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/2/7.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
