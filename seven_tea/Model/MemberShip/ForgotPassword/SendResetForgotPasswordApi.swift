b139c4a356ebacafa89f9a8a8f1d3f1ab487436c
harrison.wu <harrison.wu.global@gmail.com>
2020-02-14 15:05:17 +0800

    refs #871 modifySendResetForgotPasswordApi

--- seven_tea/Model/MemberShip/ForgotPassword/SendResetForgotPasswordApi.swift
+++ seven_tea/Model/MemberShip/ForgotPassword/SendResetForgotPasswordApi.swift
@@ -8,41 +8,50 @@
 
 import Foundation
 class SendResetForgotPasswordSmsAPI: NSObject {
-static let SendResetForgotPasswordSmsInstance = SendResetForgotPasswordSmsAPI()
-    
-    func SendResetForgotPasswordSms(Username: String , Phone : String , Validators_code : String) {
+    static let SendResetForgotPasswordSmsInstance = SendResetForgotPasswordSmsAPI()
+    var datas = Data()
+//    func getSendResetForgotPassword(Username: String , Phone : String , Validators_code : String ){
+//        let postSTring  = SendResetForgorPasswordMessage(username: Username, phone: Phone, validators_code: Validators_code)
+//        let encoder = JSONEncoder()
+//        if let data = try? encoder.encode(postSTring)
+//        {
+//            print(postSTring)
+//            self.datas = data
+//            SendResetForgotPasswordSms { (result) in
+//                if result{
+//                    return
+//                }
+//            }
+//        }
+//    }
+
+
+func SendResetForgotPasswordSms(Username: String , Phone : String , Validators_code : String , handler: @escaping (Bool)->()) {
     let url = URL(string: ApiUrl.ApiUrlInstance.SendResetForgotPasswordUrl)!
-        var request = URLRequest(url: url)
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.httpMethod = "POST"
-        let postString = "username=\(Username)&phone=\(Phone)&validators_code=\(Validators_code)"
-        request.httpBody = postString.data(using: .utf8)
-        print(postString)
-        let task = URLSession.shared.dataTask(with: request)
+    var request = URLRequest(url: url)
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
+    request.httpMethod = "POST"
+    let postString = "username=\(Username)&phone=\(Phone)&validators_code=\(Validators_code)"
+    request.httpBody = postString.data(using: .utf8)
+    print(postString)
+    let task = URLSession.shared.dataTask(with: request)
+    {
+        data, response, error in
+        //            let responseString = String(data: data!, encoding: .utf8)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        if let data = data, let SendResetPassword = try?
+            decoder.decode(SendResetForgotPasswordCodable.self, from: data)
         {
-            data, response, error in
-//            let responseString = String(data: data!, encoding: .utf8)
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            if let data = data, let SendResetPassword = try?
-                decoder.decode(SendResetForgotPasswordCodable.self, from: data)
-            {   print(data)
-                print(SendResetPassword)
-                if(SendResetPassword.success == true )
+            print(data)
+            print(SendResetPassword)
+            if(SendResetPassword.success == true )
+            {
+                //主線程
+                DispatchQueue.main.async
                 {
-                    //主線程
-                    DispatchQueue.main.async
-                    {
-                       MessageAlert.Instance.message(message: "\(SendResetPassword.message)")
-                    }
-                }
-                else
-                {
-                    //主線程
-                    DispatchQueue.main.async
-                    {
                         MessageAlert.Instance.message(message: "\(SendResetPassword.message)")
-                    }
+                        handler(true)
                 }
             }
             else
@@ -50,10 +59,20 @@ static let SendResetForgotPasswordSmsInstance = SendResetForgotPasswordSmsAPI()
                 //主線程
                 DispatchQueue.main.async
                 {
-                    MessageAlert.Instance.message(message: "資料解析錯誤")
+                        MessageAlert.Instance.message(message: "\(SendResetPassword.message)")
+                        handler(false)
                 }
             }
         }
-        task.resume()
+        else
+        {
+            //主線程
+            DispatchQueue.main.async
+            {
+                    MessageAlert.Instance.message(message: "資料解析錯誤")
+            }
+        }
     }
+    task.resume()
+}
 }
