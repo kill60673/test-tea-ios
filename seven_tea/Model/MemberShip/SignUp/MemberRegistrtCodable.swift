c54ccf6e7bcb4d9786c96677454ff9ce0af08791
harrison.wu <harrison.wu.global@gmail.com>
2020-02-14 13:59:14 +0800

    refs #6 create MemberRegiseterCodable

---
+++ seven_tea/Model/MemberShip/SignUp/MemberRegistrtCodable.swift
@@ -0,0 +1,17 @@
+//
+//  SendRegistrtInfo.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/2/10.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import Foundation
+struct SendRegisterInfoCodable: Codable {
+    var success:Bool?
+    var message :String
+}
+struct MemberRegisterMessage: Codable {
+    var phone : String
+    var validators_code: String
+}
