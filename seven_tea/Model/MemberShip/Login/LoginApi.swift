7ef9560dd9bd4f4834d56239f9575cfcc8aaba09
harrison.wu <harrison.wu.global@gmail.com>
2020-02-14 15:19:38 +0800

    refs #871 LoginApi

--- seven_tea/Model/MemberShip/Login/LoginApi.swift
+++ seven_tea/Model/MemberShip/Login/LoginApi.swift
@@ -6,39 +6,50 @@
 
 import Foundation
 import UIKit
+
+
 class LoginAPI: NSObject {
     static let LoginInstance = LoginAPI()
     var datas = Data()
+    
+    //將資料放進object的func
     func getLoginMessage(username: String,password : String)
     {
         let postSTring  = Loginmessage(username: username, password: password)
-               let encoder = JSONEncoder()
-               if let data = try? encoder.encode(postSTring)
-               {
-                print(postSTring)
-                self.datas = data
-                //Login()
-                print(data)
-               }
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(postSTring)
+        {
+            print(postSTring)
+            self.datas = data
+            print(data)
+            Login {(result) in
+                if result
+                {
+                    return
+                }
+                else
+                {
+                }
+            }
+        }
     }
     // 登入用API
-    func Login(username: String,password : String) {
+    func Login(handler: @escaping (Bool)->()) {
+        
         let url = URL(string: ApiUrl.ApiUrlInstance.LoginUrl)!
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        print(datas)
-        let postString = "username=\(username)&password=\(password)"
-        request.httpBody = postString.data(using: .utf8)
+        request.httpBody = datas
         let task = URLSession.shared.dataTask(with: request)
         {
             data, response, error in
             let responseString = String(data: data!, encoding: .utf8)
             print(responseString as Any)
             let decoder = JSONDecoder()
-            print(postString)
             decoder.dateDecodingStrategy = .iso8601
-            // 做 do catch 如果任何例外事件 就在catch print出error 
+            // 做 do catch 如果任何例外事件 就在catch print出error
             if let data = data
             {
                 do
@@ -49,25 +60,28 @@ class LoginAPI: NSObject {
                     {
                         DispatchQueue.main.async
                         {
-                            MessageAlert.Instance.message(message: "登入成功")
+                                MessageAlert.Instance.message(message: "登入成功")
+                                handler(true)
+                                
                         }
                     }
                     else
                     {
                         DispatchQueue.main.async
                         {
-                            MessageAlert.Instance.message(message: "登入失敗")
+                                MessageAlert.Instance.message(message: "登入失敗")
+                                handler(false)
                         }
                     }
                 }
                 catch
                 {
-//                    print(error)
+                    
                 }
             }
         }
         task.resume()
-    }    
+    }
 }
 
 
