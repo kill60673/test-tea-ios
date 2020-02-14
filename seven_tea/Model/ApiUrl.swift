a38c711a7da0d5fb4996c237bea632fee188943c
harrison.wu <harrison.wu.global@gmail.com>
2020-02-14 15:17:56 +0800

    refs #871 modifyApiUrl

--- seven_tea/Model/ApiUrl.swift
+++ seven_tea/Model/ApiUrl.swift
@@ -11,16 +11,23 @@ class ApiUrl: NSObject {
     static let ApiUrlInstance = ApiUrl()
     var DoMain: String = "http://seventea.test/"
     var LoginUrl:String
-    var MemberRegisterUrl:String
+    var SendMemberRegisterSmsUrl:String
     var ForgotPasswordSmsUrl:String
     var SendResetForgotPasswordUrl:String
     var RestPasswordUrl:String
+    var SignPhoneSendSmsUrl:String
+    var SendMemberRegistrUrl:String
+    var SendMemberRegisterInfoUrl:String
     
     override init() {
         LoginUrl="\(DoMain)/api/new-login"
-        MemberRegisterUrl="\(DoMain)/api/register"
+        SendMemberRegisterSmsUrl="\(DoMain)/api/register/send-phone-sms"
         ForgotPasswordSmsUrl="\(DoMain)/api/send-forgot-password"
         SendResetForgotPasswordUrl="\(DoMain)/api/send-reset-password"
         RestPasswordUrl="\(DoMain)/api/reset-password"
+        SignPhoneSendSmsUrl="\(DoMain)/api/register/send-phone-sms"
+        SendMemberRegistrUrl="\(DoMain)/api/register/register-button"
+        SendMemberRegisterInfoUrl="\(DoMain)/api/register/send-register-info"
+        
     }
 }
