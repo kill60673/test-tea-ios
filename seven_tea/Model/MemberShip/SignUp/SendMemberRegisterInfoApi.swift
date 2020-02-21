23c67c99d6030a60afae3828fd0723ce30cb81ba
harrison.wu <harrison.wu.global@gmail.com>
2020-02-21 17:10:16 +0800

    refs #901 modify SemdMemberRegisterInfoApi

--- seven_tea/Model/MemberShip/SignUp/SendMemberRegisterInfoApi.swift
+++ seven_tea/Model/MemberShip/SignUp/SendMemberRegisterInfoApi.swift
@@ -8,28 +8,15 @@ class SendMemberRegisterInfoAPI : NSObject {
     static let MemberRegisterInstance = SendMemberRegisterInfoAPI()
     var datas = Data()
     
-    //將資料放進object的func
-//    func getMemberRegisterMessageInfo(Username: String , Name : String ,Sex : String , Phone: String ,Password : String,  Password_confirmation: String , Validators_code: String)
-//    {
-//        let postSTring  = MemberRegisterMessageInfo(username: username, name: name, sex: sex, phone: phone, password: password, password_confirmation: password_confirmation, validators_code: validators_code)
-//               let encoder = JSONEncoder()
-//               if let data = try? encoder.encode(postSTring)
-//               {
-//                print(postSTring)
-//                self.datas = data
-//                print(data)
-//                SendMemberRegisterInfo()
-//               }
-//
-//    }
-    func SendMemberRegisterInfo(Username : String , Name : String , Sex : String , Phone: String , Password : String ,  Password_confirmation : String , Validators_code : String) {
+    
+    func SendMemberRegisterInfo( handler : @escaping (Bool)->()) {
         let url = URL(string : ApiUrl.ApiUrlInstance.SendMemberRegisterInfoUrl)!
         var request = URLRequest(url : url)
         request.setValue("application/json", forHTTPHeaderField : "Accept")
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        //        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        let postString = "username=\(Username)&name=\(Name)&sex=\(Sex)&phone=\(Phone)&password=\(Password)&password_confirmation=\(Password_confirmation)&validators_code=\(Validators_code)"
-        request.httpBody = postString.data(using : .utf8)
+        let postString = datas
+        request.httpBody = postString
         let task = URLSession.shared.dataTask(with : request)
         {
             data, response, error in
@@ -43,16 +30,16 @@ class SendMemberRegisterInfoAPI : NSObject {
                 {
                     //主線程
                     DispatchQueue.main.async
-                    {
-                        MessageAlert.Instance.message(message : "\(MemberRegisterInfo.message)")
+                        {
+                            MessageAlert.Instance.message(message : "\(MemberRegisterInfo.message)")
                     }
                 }
                 else
                 {
                     //主線程
                     DispatchQueue.main.async
-                    {
-                        MessageAlert.Instance.message(message : "\(MemberRegisterInfo.message)")
+                        {
+                            MessageAlert.Instance.message(message : "\(MemberRegisterInfo.message)")
                     }
                 }
             }
@@ -60,8 +47,8 @@ class SendMemberRegisterInfoAPI : NSObject {
             {
                 //主線程
                 DispatchQueue.main.async
-                {
-                    
+                    {
+                        
                 }
             }
         }
