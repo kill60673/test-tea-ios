c64bcfbb18a5190844f7bfe1ccf9e59eedd641c0
harrison.wu <harrison.wu.global@gmail.com>
2020-03-03 18:07:45 +0800

    refs #996 , create membercenterviewcontroller

--- seven_tea/ViewController/MenberShipViewController/SingUpViewController/MemberCenterViewController/MenberCenterViewController.swift
+++ seven_tea/ViewController/MenberShipViewController/SingUpViewController/MemberCenterViewController/MenberCenterViewController.swift
@@ -5,23 +5,36 @@
 //
 
 import UIKit
+var memberInfoView: UIViewController!
 
 class MenberCenterViewController: UIViewController {
-
+    @IBOutlet weak var memberView: UIView!
+    @IBOutlet weak var memberTableView: UITableView!
+    @IBOutlet weak var btLogout: UIButton!
+    @IBOutlet weak var lbName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        memberInfoView = self
+        memberView.isHidden = true
+        memberTableView.isHidden = true
+        btLogout.customized_button(button: btLogout)
+        keyboad()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btLogout(_ sender: Any) {
+        UserInfo.UserInfoInstance.delete()
+        memberInfoView.dismiss(animated: true, completion: nil)
+        memberInfoView.navigationController?.popViewController(animated: true)
+        self.tabBarController?.selectedIndex = 0
+               MessageAlert.Instance.message(message: "已登出")
     }
-    */
-
+    override func viewDidAppear(_ animated: Bool) {
+        if UserInfo.UserInfoInstance.preferences.object(forKey: "token") != nil {
+            memberView.isHidden = false
+                       memberTableView.isHidden = false
+                       lbName.text = UserInfo.UserInfoInstance.preferences.object(forKey: "name") as? String
+        } else {
+            memberView.isHidden = true
+            memberTableView.isHidden = true
+        }
+    }
 }
