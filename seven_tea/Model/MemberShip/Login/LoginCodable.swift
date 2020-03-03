fa6d916ecf1c0ad7c5c3e57d3223123f4115bd69
harrison.wu <harrison.wu.global@gmail.com>
2020-03-03 16:46:35 +0800

    refs #996 , create memberinfo and login save token and memberinfo

--- seven_tea/Model/MemberShip/Login/LoginCodable.swift
+++ seven_tea/Model/MemberShip/Login/LoginCodable.swift
@@ -10,7 +10,11 @@ import Foundation
 struct LoginCodable: Codable {
     var success: Bool?
     var message: String
+    var data: Datas
 }
+struct Datas: Codable {
+       var token: String
+   }
 struct Loginmessage: Codable {
     var username: String
     var password: String
