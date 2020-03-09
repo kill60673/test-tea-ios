39867ead226ef44a46c73a89f3fa04a26b8c6f7a
harrison.wu <harrison.wu.global@gmail.com>
2020-03-09 12:14:10 +0800

    refs #70 modifyLoginApi

--- seven_tea/Model/MemberShip/Login/LoginApi.swift
+++ seven_tea/Model/MemberShip/Login/LoginApi.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class LoginAPI: NSObject {
     static let LoginInstance = LoginAPI()
-
+    
     //將資料放進object的func
     // 登入用API
     func login(handler : @escaping (Bool) -> Void) {
-
+        
         let url = URL(string: ApiUrl.ApiUrlInstance.loginUrl)!
         var request = URLRequest(url: url)
-
+        
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
@@ -24,7 +24,7 @@ class LoginAPI: NSObject {
         let task = URLSession.shared.dataTask(with: request) {
             data, _, _ in
             let responseString = String(data: data!, encoding: .utf8)
-//            print(responseString as Any)
+            //            print(responseString as Any)
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .iso8601
             // 做 do catch 如果任何例外事件 就在catch print出error
@@ -34,19 +34,24 @@ class LoginAPI: NSObject {
                     if login.success == true {
                         DispatchQueue.main.async {
                             MemberInfoApi.MemberInfoInstance.memberInfo(token: "Bearer \(login.data.token)")
-//                            print(login.data.token)
+                            loginView.dismiss(animated: true, completion: nil)
+                            loginView.navigationController?.popViewController(animated: true)
+                            //tab 切換的第0個
+                            loginView.tabBarController?.selectedIndex = 0
+                            
+                            //                            print(login.data.token)
                             MessageAlert.Instance.message(message: login.message)
-                                handler(true)
+                            handler(true)
                         }
                     } else {
                         DispatchQueue.main.async {
-//                            print(login.data)
+                            //                            print(login.data)
                             MessageAlert.Instance.message(message: login.message)
-                                handler(false)
+                            handler(false)
                         }
                     }
                 } catch {
-
+                    
                 }
             }
         }
