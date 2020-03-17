73c290192003c35f1d33ad0781025051a4d160f2
harrison.wu <harrison.wu.global@gmail.com>
2020-03-17 14:42:13 +0800

    refs #1099 , add pickImageAuthority

--- seven_tea/ViewController/MenberShipViewController/MemberCenterViewController/FrequentlyAskedQuestions/AddFeedBackViewController.swift
+++ seven_tea/ViewController/MenberShipViewController/MemberCenterViewController/FrequentlyAskedQuestions/AddFeedBackViewController.swift
@@ -8,34 +8,55 @@
 
 import UIKit
 
-class AddFeedBackViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate ,UITextViewDelegate {
-  
+class AddFeedBackViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate ,UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
     @IBOutlet weak var tfTitle: UITextField!
     @IBOutlet weak var tvContent: UITextView!
     @IBOutlet weak var pvType: UIPickerView!
     @IBOutlet weak var btSubmit: UIBarButtonItem!
-    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var UIImageView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
     var type = "未選擇意見類型"
     override func viewDidLoad() {
         super.viewDidLoad()
         tfTitle.delegate = self
         tvContent.delegate = self
         tvContent.setframe(textview: tvContent)
-        imageView.setframe(view: imageView)
+        UIImageView.setframe(view: UIImageView)
     }
     @IBAction func btSubmit(_ sender: Any) {
         //送出判斷式連結API
     }
     @IBAction func btPickImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        /* 將UIImagePickerControllerDelegate、UINavigationControllerDelegate物件指派給UIImagePickerController */
+        imagePicker.delegate = self
+        /* 照片來源為相簿 */
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
         
     }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        /* 利用指定的key從info dictionary取出照片 */
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /* 挑選照片過程中如果按了Cancel，關閉挑選畫面 */
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-          return 1
-      }
-      
-      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return feedbacktype.count
-      }
+    }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return feedbacktype[row]
     }
