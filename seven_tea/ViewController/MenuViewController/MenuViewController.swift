09f13b29407c0e9d80351a7d90f28252706e0a99
harrison.wu <harrison.wu.global@gmail.com>
2020-03-26 17:51:25 +0800

    refs #9 create MuneViewController and create OrderView

---
+++ seven_tea/ViewController/MenuViewController/MenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MenuViewController.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/3/26.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuTableViewCell
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
