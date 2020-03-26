bce14fc4afb122b7e18ca0e031e4077158922524
harrison.wu <harrison.wu.global@gmail.com>
2020-03-26 18:06:20 +0800

    refs #11 , create ShopCarViewController

---
+++ seven_tea/ViewController/ShopCarViewController/ShopCarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderTableViewCell.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/3/26.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
