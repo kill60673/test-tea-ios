4ba0ec0f7dc2d4fae4a79fb51061f1a423a44010
harrison.wu <harrison.wu.global@gmail.com>
2020-03-26 16:35:38 +0800

    refs #8 modify pickCountyAlert

--- seven_tea/OtherController/HelpController/AppOverAllStyle/AppOverAllStyle.swift
+++ seven_tea/OtherController/HelpController/AppOverAllStyle/AppOverAllStyle.swift
@@ -52,4 +52,17 @@ extension UIViewController {
         myAlert.addAction(cancelAction)
         present(myAlert, animated: true, completion: nil)
     }
+    func openlise(){
+        let vc = UIViewController()
+               vc.preferredContentSize = CGSize(width: 250,height: 300)
+               let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 250, height: 300))
+               pickerView.delegate = self as? UIPickerViewDelegate
+               pickerView.dataSource = self as? UIPickerViewDataSource
+               vc.view.addSubview(pickerView)
+               let editRadiusAlert = UIAlertController(title: "Choose distance", message: "", preferredStyle: UIAlertController.Style.alert)
+               editRadiusAlert.setValue(vc, forKey: "contentViewController")
+               editRadiusAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
+               editRadiusAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+               self.present(editRadiusAlert, animated: true)
+    }
 }
--- seven_tea/OtherController/HelpController/UIelementsController/UIelementsController.swift
+++ seven_tea/OtherController/HelpController/UIelementsController/UIelementsController.swift
@@ -14,7 +14,7 @@ extension UIButton {
         button.layer.borderWidth = 1.0//外框粗度
         button.layer.cornerRadius = 5//圓角
     }
-
+    
     func round_button(button: UIButton) {
         button.layer.borderWidth = 1.0//外框粗度
         button.layer.cornerRadius = 20//圓角
@@ -24,12 +24,23 @@ extension UITextView {
     func setframe(textview: UITextView) {
         textview.layer.borderWidth = 1.0
         textview.layer.cornerRadius = 5.0
-
+        
     }
 }
 extension UIView {
     func setframe(view: UIView) {
         view.layer.borderWidth = 1.0
         view.layer.cornerRadius = 5.0
+    }
+    func setView(View:UIView){
+        View.translatesAutoresizingMaskIntoConstraints = false
+        View.heightAnchor.constraint(equalToConstant: 128).isActive = true
+        View.leadingAnchor.constraint(equalToSystemSpacingAfter: View.leadingAnchor, multiplier: 0).isActive = true
+        View.trailingAnchor.constraint(equalToSystemSpacingAfter: View.trailingAnchor, multiplier: -10).isActive = true
+        let c = View.bottomAnchor.constraint(equalToSystemSpacingBelow: View.bottomAnchor, multiplier: 128)
+        c.identifier = "bottom"
+        c.isActive = true
+        View.layer.cornerRadius = 10
+        View.layer.cornerRadius = 10
     }
 }
