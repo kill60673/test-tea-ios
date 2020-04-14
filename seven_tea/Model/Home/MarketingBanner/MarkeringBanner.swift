6c90223ab40845063a3cfbfe7f8795ba0279b136
harrison.wu <harrison.wu.global@gmail.com>
2020-04-14 15:22:40 +0800

    refs #7 create MarketingBannerObject

---
+++ seven_tea/Model/Home/MarketingBanner/MarkeringBanner.swift
@@ -0,0 +1,21 @@
+//
+//  MarkeringBanner.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/4/14.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import Foundation
+
+class MarkeringBanner: NSObject {
+    var imageType = String()
+    var pictureURL = String()
+    var linkURL = String()
+    
+    init(imageType: String, pictureURL: String, linkURL: String) {
+        self.imageType = imageType
+        self.pictureURL = pictureURL
+        self.linkURL = linkURL
+    }
+}
