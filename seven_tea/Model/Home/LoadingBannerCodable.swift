6bf648797ab9bb76a6c5e48a1d512cb57e7c1d6e
harrison.wu <harrison.wu.global@gmail.com>
2020-04-14 15:09:37 +0800

    refs #7 add loadingbannerCodable

---
+++ seven_tea/Model/Home/LoadingBannerCodable.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingBannerCodable.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/4/14.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import Foundation
+struct LoadingBanner: Codable {
+    var success: Bool
+    var message: String
+    var data: [data]
+    
+    struct data: Codable {
+        var img_type: String
+        var picture_url: String
+        var link_url: String
+    }
+}
