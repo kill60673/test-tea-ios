f746b1cac83706beb7bfe6678faeb2ba41b097a6
harrison.wu <harrison.wu.global@gmail.com>
2020-04-14 15:27:04 +0800

    refs #7 create PromotionBannerObject

---
+++ seven_tea/Model/Home/PromotionBanner/PromotionBanner.swift
@@ -0,0 +1,16 @@
+//
+//  PromotionBanner.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/4/14.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import Foundation
+class PromotionBanner: NSObject {
+    var pictureURL = String()
+
+    init(pictureURL: String) {
+        self.pictureURL = pictureURL
+    }
+}
