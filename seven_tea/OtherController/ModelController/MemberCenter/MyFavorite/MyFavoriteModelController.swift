909d9e7cd950c61b6d6f4f9ff635df724c35e0da
harrison.wu <harrison.wu.global@gmail.com>
2020-05-06 17:18:29 +0800

    refs #70 create addtoFavoriteitem

--- seven_tea/OtherController/ModelController/MemberCenter/MyFavorite/MyFavoriteModelController.swift
+++ seven_tea/OtherController/ModelController/MemberCenter/MyFavorite/MyFavoriteModelController.swift
@@ -5,3 +5,24 @@
 //
 
 import Foundation
+import UIKit
+func getAddFavoriteMessage(item_id: Int, item_name: String, item_category: Int, tmp: String, sugar: String, size: String, add: [String], store_id: Int, price: Int) {
+    let postString  = AddMyFavorite(item_id: item_id, item_name: item_name, item_category: item_category, tmp: tmp, sugar: sugar, size: size, add: add, store_id: store_id, price: price)
+    let encoder = JSONEncoder()
+    if let data = try? encoder.encode(postString) {
+        print(postString)
+        datas = data
+        print(data)
+        if UserInfo.UserInfoInstance.preferences.object(forKey: "token") as? String == nil {
+            print("is nil")
+        }
+        print("dddd",UserInfo.UserInfoInstance.preferences.object(forKey: "token") as! String)
+        AddMyFavoriteApi.AddMyFavoriteInstance.addmyfavorite(token: UserInfo.UserInfoInstance.preferences.object(forKey: "token") as! String) {(result) in
+            if result {
+                   
+                return
+            } else {
+            }
+        }
+    }
+}
