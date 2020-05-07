52fc607163eeb213b4831d3eafb1fabbe52ea964
harrison.wu <harrison.wu.global@gmail.com>
2020-05-07 14:15:06 +0800

    refs #43 create getshoppingcarapi

---
+++ seven_tea/Model/ShoppingCar/GetShoppingDetail.swift
@@ -0,0 +1,23 @@
+//
+//  GetShoppingDetail.swift
+//  seven_tea
+//
+//  Created by harrison on 2020/5/7.
+//  Copyright © 2020 harrison公司機. All rights reserved.
+//
+
+import Foundation
+class GetShoppingCarDetail{
+    var store_id: Int
+    var store_name: String
+    var totle_price: Int
+    var can_delivery: Bool
+    var gap_to_delivery: Int
+    init(store_id:Int,store_name:String,totle_price:Int,can_delivery:Bool,gap_to_delivery:Int) {
+        self.store_id = store_id
+        self.store_name = store_name
+        self.totle_price = totle_price
+        self.can_delivery = can_delivery
+        self.gap_to_delivery = gap_to_delivery
+    }
+}
